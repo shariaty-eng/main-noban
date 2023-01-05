3a1b83fd69c3d4a132f23d72a849dc407391e614
f.shariaty <f.shariaty0067@gmail.com>
2023-01-05 14:01:24 +0330

    the verification component modified
    this component receives a four-digit code from the user and also includes two buttons to confirm and delete the code

--- src/routes/Verification.jsx
+++ src/routes/Verification.jsx
@@ -1,8 +1,84 @@
-const Verifications = () => {
+import { useEffect, useRef, useState } from "react";
+import PinInput from "react-pin-input";
+import { AiOutlineClose } from 'react-icons/ai'
+import "../assets/style/verification.css"
+
+const Verification = () => {
+
+    const initState = {
+        validateCode: null,
+        validatePhone: 'correct phone number',
+    }
+
+
+    const [state, setState] = useState(initState)
+    const validateCode = useRef();
+    useEffect(() => {
+        validateCode.current.focus();
+    }, [])
+
+    const clearInput = () => {
+        setState((prevState) => ({
+            ...prevState,
+            validateCode: '',
+        }))
+    }
+
     return (
-        <h1>Verifications</h1>
-    )
+
+        <div className="verification-segment">
+            <div className="description">
+                Enter your 4 digits verification code
+            </div>
+            <div className="InputDigitNumber-container mt-4">
+                <PinInput
+                    length={4}
+                    maxLength="1"
+                    minLength="1"
+                    type="numeric"
+                    inputMode="number"
+                    ref={validateCode}
+                    //  initialValue={state.validateCode}
+                    autoSelect={true}
+                    name="validateCode"
+                    // onChange={handleCodeChange}
+                    inputStyle={{
+                        background: '#f7f7f7',
+                        boxShadow: "none",
+                        border: "none",
+                        borderRadius: "4px",
+                        borderBottom: "1px solid #979797",
+                        boxSizing: "border-box",
+                        margin: "5px",
+                        height: "63px",
+                        width: "45px",
+                        textAlign: "center",
+                        fontSize: "28px",
+                        color: "#4d4d4d",
+                    }}
+                    inputFocusStyle={{ background: '#f7f7f7' }}
+                />
+            </div>
+
+            <div className="d-flex flex-row justify-content-end align-items-center mt-4">
+                <button className="btn btn-outline-primary mx-3">VERIFY CODE</button>
+                <button className="btn btn-outline-danger" onClick={clearInput}><AiOutlineClose size={15} /> CLEAR</button>
+            </div>
+            {/* <div>
+        <input
+        type="text"
+        name="validatePhone"
+        value={state.validatePhone}
+        className="input_style"
+        />
+
+      </div> */}
+
+        </div>
+
+
+    );
 
 }
 
-export default Verifications
+export default Verification
